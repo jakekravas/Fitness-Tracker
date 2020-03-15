8d956d189d4fe964b7f4b63c20c6cbec494b3fd8
Jake Kravas <jakekravas7@gmail.com>
2020-03-14 23:52:59 -0700

    fixed issues with a couple api routes and added a couple new ones

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,39 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 let db = require("../models");
 
 module.exports = function(app){
-    // app.get("/api/workouts/range", (req, res) => {
-    //     db.Workout.find({}).then(data => {
-    //         res.json(data);
-    //     });
-    // })
     app.get("/api/workouts", (req, res) => {
-        db.Workout.populate({}).then(data => {
+        db.Workout.find({}, (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        })
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        let year = new Date().getFullYear();
+        let month = new Date().getMonth();
+        let date = new Date().getDate();
+        let weekPriorDate = new Date(year, month, date - 7);
+
+        db.Workout.find({day: {$gte: weekPriorDate}}, (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        })
+
+    })
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.updateOne(
+            {_id: req.params.id},
+            {$push: {exercises: req.body}})
+            .then(data => {
+                res.json(data);
+            });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body).then(data => {
             res.json(data);
         });
-    })
+    });
 }
--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 1028;
 const app = express();
 
-const db = require("./models");
-
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
